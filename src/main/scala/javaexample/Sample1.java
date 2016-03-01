cd3dc8f5894252accc5c78ac963d4fbd0b2713f1
smirklijie <smirklijie@qq.com>
2016-03-01 15:30:58 +0800

    javab本地方法，与scala比较测试

---
+++ src/main/scala/javaexample/Sample1.java
@@ -0,0 +1,23 @@
+package javaexample;
+
+class Sample1 {
+    // --- Native methods
+    public native int intMethod(int n);
+    public native boolean booleanMethod(boolean bool);
+    public native String stringMethod(String text);
+    public native int intArrayMethod(int[] intArray);
+
+    // --- Main method to test our native library
+    public static void main(String[] args) {
+        System.loadLibrary("Sample1");
+        Sample1 sample = new Sample1();
+        int square = sample.intMethod(5);
+        boolean bool = sample.booleanMethod(true);
+        String text = sample.stringMethod("java");
+        int sum = sample.intArrayMethod(new int[] {1, 1, 2, 3, 5, 8, 13});
+        System.out.println("intMethod: " + square);
+        System.out.println("booleanMethod: " + bool);
+        System.out.println("stringMethod: " + text);
+        System.out.println("intArrayMethod: " + sum);
+    }
+}
